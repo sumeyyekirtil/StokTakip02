f396721d1538f03c73028f7ca7ed3045d16cedf7
sumeyye <sumeyyekirtil4@gmail.com>
2025-06-27 22:43:10 +0300

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,11 @@
+﻿namespace StokTakip02
+{
+	public class Product : IEntity
+	{
+		public int ProductId { get; set; }
+		public string Department { get; set; }
+		public int Upper { get; set; }
+		public int Lower { get; set; }
+		public DateTime CreatDate { get; set; }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace StokTakip02
+{
+    class MenClothes : Product
+    {
+		public int Pants { get; set; }
+		public int Coat { get; set; }
+		public int Cravat { get; set; }
+		public int Heels { get; set; }
+	}
+    class WomanClothes : Product
+    {
+		public int HighHeels { get; set; }
+		public int Jewelry { get; set; }
+		public int Case { get; set; }
+	}
+    class KidsClothes : Product
+    {
+		public int Toys { get; set; }
+		public int Shoes { get; set; }
+	}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("StokTakip02-Clothes!");
+        }
+    }
+}
